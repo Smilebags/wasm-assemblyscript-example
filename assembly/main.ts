dfb263145694cf7611a22485af956c8ce7b976a4
Chris Cook <christopher.cook@outlook.com.au>
2018-12-27 19:15:29 +1100

    Add some benchmarking comparison functions

--- assembly/main.ts
+++ assembly/main.ts
@@ -1,6 +1,8 @@
-declare function sayHello(): void;
+import "allocator/arena";
 
-sayHello();
+// declare function sayHello(): void;
+
+// sayHello();
 
 export function add(x: i32, y: i32): i32 {
   return x + y;
@@ -8,4 +10,50 @@ export function add(x: i32, y: i32): i32 {
 
 export function power(base: i32, exponent: i32): i32 {
   return (base ** exponent) as i32;
+}
+
+class MemoItem {
+  count: i32;
+  result: i32;
+}
+
+let fibMemo: MemoItem[] = [];
+
+function memoExists(count: i32): i32 {
+  for (let i = 0; i < fibMemo.length; i++) {
+    let element: MemoItem = fibMemo[i];
+    if(element.count === count) {
+      return element.result;
+    }
+  }
+  return -1;
+}
+export function fib(count: i32): i32 {
+  if(count === 1 || count === 2) {
+    return 1;
+  } else {
+    let memoEntry: i32 = memoExists(count);
+    if(memoEntry !== -1) {
+      return memoEntry;
+    } else {
+      let result: i32 = fib(count-1) + fib(count - 2);
+      fibMemo.push({count, result});
+      return result;
+    }
+  }
+}
+
+export function fastFib(count: i32): f64 {
+  if(count === 1 || count === 2) {
+    return 1;
+  } else {
+    let a: f64 = 1;
+    let b: f64 = 1;
+    for (let i = 3; i <= count; i++) {
+      let temp: f64 = a + b;
+      b = a;
+      a = temp;
+    }
+    return a;
+  }
 }
--- src/main.js
+++ src/main.js
@@ -18,19 +18,19 @@
 
 /**
  * returns the exports from a wasm module after compiling and extracting the exports
- * @param moduleUrl: Path to the wasm module file
- * @param imports: an object of functions to import into the WASM module
+ * @param moduleUrl Path to the wasm module file
+ * @param imports an object of functions to import into the WASM module
  */
 async function getWasmModule(moduleUrl, imports) {
   let env = {
     abort: (msg, file, line, column) => {
-      console.error("abort called at main.ts:" + line + ":" + column);
     }
   };
   return new Promise((resolve, reject) => {
-    WebAssembly.instantiateStreaming(fetch("../out/main.wasm"), {main: imports, env }).then((result) => {
+    WebAssembly.instantiateStreaming(fetch("/out/main.wasm"), {main: imports, env }).then((result) => {
       let exportObj = {};
       Object.keys(result.instance.exports).forEach((key) => {
+        console.log(`Attaching ${key} to export`);
         exportObj[key] = result.instance.exports[key];
       });
       resolve(exportObj);
@@ -38,13 +38,86 @@ async function getWasmModule(moduleUrl, imports) {
   });
 }
 
+
+
 (async function(){
-  let imports = {
-    sayHello: () => {
-      console.log("Hello from WebAssembly!");
-    }
+  // let imports = {
+  //   sayHello: () => {
+  //     console.log("Hello from WebAssembly!");
+  //   }
+  // };
+  let {add, power, fib: wasmFib, fastFib: wasmFastFib} = await getWasmModule("../out/main.wasm", {});
+  for (let i = 1000000; i <= 1000005; i++) {
+    perfCompare(
+      () => console.log(wasmFastFib(i)),
+      () => console.log(jsFastFib(i)),
+      `wa-${i}`,
+      `js-${i}`,
+    );
   };
-  let {add, power} = await getWasmModule("../out/main.wasm", imports);
-  document.getElementById("add").textContent = "Result: " + add(19, 23);
-  document.getElementById("power").textContent = "Result: " + power(2, 8);
+    
 })();
+
+
+function jsFib(count) {
+  if(count === 1) {
+    return 1;
+  } else if(count === 2) {
+    return 2;
+  } else {
+    return jsFib(count-1) + jsFib(count - 2);
+  }
+}
+
+let jsMemo = [];
+
+function memoExists(count) {
+  for (let i = 0; i < jsMemo.length; i++) {
+    let element = jsMemo[i];
+    if(element.count === count) {
+      return element.result;
+    }
+  }
+  return -1;
+}
+
+function memoJsFib(count) {
+  if(count === 1) {
+    return 1;
+  } else if(count === 2) {
+    return 2;
+  } else {
+    let memoEntry = memoExists(count);
+    if(memoEntry !== -1) {
+      return memoEntry;
+    } else {
+      let result = memoJsFib(count-1) + memoJsFib(count - 2);
+      jsMemo.push({count, result});
+      return result;
+    }
+  }
+}
+
+function jsFastFib(count) {
+  if(count === 1 || count === 2) {
+    return 1;
+  } else {
+    let a = 1;
+    let b = 1;
+    for (let i = 3; i <= count; i++) {
+      let temp = a + b;
+      b = a;
+      a = temp;
+    }
+    return a;
+  }
+}
+
+function perfCompare(f1, f2, f1Name = "Function 1", f2Name = "Function 2") {
+  console.time(f1Name);
+  f1();
+  console.timeLog(f1Name);  
+  console.time(f2Name);
+  f2();
+  console.timeLog(f2Name);
+}
